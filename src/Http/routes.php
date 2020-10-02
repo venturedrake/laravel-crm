ec440e5816eca7c44fea913bece951e9d21972b5
Andrew Drake <andrewdrake@users.noreply.github.com>
2020-10-02 01:41:12 +0000

    Apply fixes from StyleCI

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
     Route::get('crm', function () {
-    return response('crm route reserved for Laravel CRM', 200)
+        return response('crm route reserved for Laravel CRM', 200)
         ->header('Content-Type', 'text/plain');
-})->name('laravelcrm.index');
+    })->name('laravelcrm.index');
