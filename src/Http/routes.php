2c383830798a6fb9ec3d94e573fb1330bd9af21d
Andrew Drake <andrew@venturedrake.com>
2020-10-02 11:41:03 +1000

    Updated travis settings

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('crm', function () {
+    Route::get('crm', function () {
     return response('crm route reserved for Laravel CRM', 200)
         ->header('Content-Type', 'text/plain');
 })->name('laravelcrm.index');
