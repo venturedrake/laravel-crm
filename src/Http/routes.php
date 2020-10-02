fa46cb45c2c9a00e4b13e0de0fc9679b5e0eb3bd
Andrew Drake <andrew@venturedrake.com>
2020-10-02 11:48:50 +1000

    use Route added

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,8 @@
 <?php
 
-    Route::get('crm', function () {
-        return response('crm route reserved for Laravel CRM', 200)
-        ->header('Content-Type', 'text/plain');
-    })->name('laravelcrm.index');
+use Illuminate\Support\Facades\Route;
+
+Route::get('crm', function () {
+    return response('crm route reserved for Laravel CRM', 200)
+    ->header('Content-Type', 'text/plain');
+})->name('laravelcrm.index');
