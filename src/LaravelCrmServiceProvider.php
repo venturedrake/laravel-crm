2f8c4099f26c8aa27627fe5dec973642047510c2
Andrew Drake <andrew@venturedrake.com>
2020-10-07 22:05:36 +1100

    Apply fixes from StyleCI (#11)

--- src/LaravelCrmServiceProvider.php
+++ src/LaravelCrmServiceProvider.php
@@ -45,9 +45,9 @@ class LaravelCrmServiceProvider extends ServiceProvider
                 $this->publishes([
                     __DIR__ . '/../database/migrations/create_laravel_crm_tables.php.stub' => database_path(
                         'migrations/'.date(
-                        'y_m_d_His',
-                        time().'_create_laravel_crm_tables.php'
-                    )
+                            'y_m_d_His',
+                            time().'_create_laravel_crm_tables.php'
+                        )
                     ),
                 ], 'migrations');
             }
