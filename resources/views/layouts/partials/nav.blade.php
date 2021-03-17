27cd8fb1d6d1f55429421fe01abb6174654c83ec
Andrew Drake <andrew@venturedrake.com>
2021-03-18 10:28:07 +1100

    Hide seperator

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -11,7 +11,7 @@
             <li class="nav-item"><a class="nav-link {{ (strpos(Route::currentRouteName(), 'laravel-crm.organisations') === 0) ? 'active' : '' }}" href="{{ url(route('laravel-crm.organisations.index')) }}">Organisations</a></li>
             <li class="nav-item"><a class="nav-link {{ (strpos(Route::currentRouteName(), 'laravel-crm.users') === 0) ? 'active' : '' }}" href="{{ url(route('laravel-crm.users.index')) }}">Users</a></li>
             <li class="nav-item"><a class="nav-link {{ (strpos(Route::currentRouteName(), 'laravel-crm.teams') === 0) ? 'active' : '' }}" href="{{ url(route('laravel-crm.teams.index')) }}">Teams</a></li>
-            <li class="dropdown-divider"></li>
+            {{--<li class="dropdown-divider"></li>--}}
            {{-- <li class="nav-item"><a class="nav-link" href="#">Email</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Documents</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>--}}
