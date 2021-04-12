af28c58210e7824a53546602faedb981177ba623
Andrew Drake <andrew@venturedrake.com>
2021-04-12 15:01:54 +1000

    Require use role

--- src/Http/Requests/StoreUserRequest.php
+++ src/Http/Requests/StoreUserRequest.php
@@ -27,6 +27,7 @@ class StoreUserRequest extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email',
             'password' => 'required|string|min:8|confirmed',
+            'role' => 'required',
         ];
     }
 }
--- src/Http/Requests/UpdateUserRequest.php
+++ src/Http/Requests/UpdateUserRequest.php
@@ -26,6 +26,7 @@ class UpdateUserRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email,'.$this->user->id,
+            'role' => 'required',
         ];
     }
 }
