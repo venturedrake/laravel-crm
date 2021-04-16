662d5ba05bd31824fd93c99ed0a00e7d0b11feec
Andrew Drake <andrew@venturedrake.com>
2021-04-16 10:00:02 +1000

    Removed button

--- resources/views/updates/index.blade.php
+++ resources/views/updates/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
     <div class="card">
-        <div class="card-header"><h3 class="card-title float-left m-0">Laravel CRM Updates</h3> <span class="float-right"><a type="button" class="btn btn-primary btn-sm" href="{{ url(route('laravel-crm.teams.create')) }}"><span class="fa fa-plus"></span> Add team</a></span></div>
+        <div class="card-header"><h3 class="card-title m-0">Laravel CRM Updates</h3></div>
         <div class="card-body">
-            <p class="card-text">Current version {{ \VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value }} {{ (\VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value == \VentureDrake\LaravelCrm\Models\Setting::where('name','version_latest')->first()->value) ? 'is the latest version' : null }}.</p>
+            <p class="card-text">Current version {{ \VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value }} {{ (\VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value == \VentureDrake\LaravelCrm\Models\Setting::where('name','version_latest')->first()->value) ? 'is the latest version' : null }}</p>
             @if(\VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value < \VentureDrake\LaravelCrm\Models\Setting::where('name','version_latest')->first()->value)
                 <hr />
                 <h5 class="mb-4"> Updated version of Laravel CRM is available.</h5>
