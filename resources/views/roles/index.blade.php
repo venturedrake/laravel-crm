a4f95d8f8686371b81e72cd86b7c9ecdd2239dfb
Andrew Drake <andrew@venturedrake.com>
2021-07-28 15:23:05 +1000

    Updated blade directives in views for role permissions

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -9,7 +9,7 @@
         <div class="card-body">
             <div class="tab-content">
                 <div class="tab-pane active" id="roles" role="tabpanel">
-                    <h3 class="mb-3"> {{ ucfirst(__('laravel-crm::lang.roles')) }}  <span class="float-right"><a type="button" class="btn btn-primary btn-sm" href="{{ url(route('laravel-crm.roles.create')) }}"><span class="fa fa-plus"></span>  {{ ucfirst(__('laravel-crm::lang.add_role')) }}</a></span></h3>
+                    <h3 class="mb-3"> {{ ucfirst(__('laravel-crm::lang.roles')) }}  @can('create crm roles')<span class="float-right"><a type="button" class="btn btn-primary btn-sm" href="{{ url(route('laravel-crm.roles.create')) }}"><span class="fa fa-plus"></span>  {{ ucfirst(__('laravel-crm::lang.add_role')) }}</a></span>@endcan</h3>
                     <div class="table-responsive">
                         <table class="table mb-0 card-table table-hover">
                             <thead>
@@ -33,13 +33,19 @@
                                     <td>{{ $role->updated_at->toFormattedDateString() }}</td>
                                     <td>{{ $role->users->count() }}</td>
                                     <td class="disable-link text-right">
+                                        @can('view crm roles')
                                         <a href="{{  route('laravel-crm.roles.show',$role) }}" class="btn btn-outline-secondary btn-sm"><span class="fa fa-eye" aria-hidden="true"></span></a>
+                                        @endcan
+                                        @can('edit crm roles')
                                         <a href="{{  route('laravel-crm.roles.edit',$role) }}" class="btn btn-outline-secondary btn-sm {{ (in_array($role->name, ['Owner','Admin'])) ? 'disabled' : null }}" ><span class="fa fa-edit" aria-hidden="true"></span></a>
+                                        @endcan
+                                        @can('delete crm roles')
                                         <form action="{{ route('laravel-crm.roles.destroy',$role) }}" method="POST" class="form-check-inline mr-0 form-delete-button">
                                             {{ method_field('DELETE') }}
                                             {{ csrf_field() }}
                                             <button class="btn btn-danger btn-sm" type="submit" data-model="{{ __('laravel-crm::lang.role') }}" {{ (in_array($role->name, ['Owner','Admin']) || $role->users->count() >= 1) ? 'disabled' : null }}><span class="fa fa-trash-o" aria-hidden="true"></span></button>
                                         </form>
+                                        @endcan    
                                     </td>
                                 </tr>
                             @endforeach
