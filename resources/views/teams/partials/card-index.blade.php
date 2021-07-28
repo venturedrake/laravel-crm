7e8a262650e970ef69f7913af4c716e5154cfa0e
Andrew Drake <andrew@venturedrake.com>
2021-07-28 15:01:45 +1000

    Updated blade directives in views for team permissions

--- resources/views/teams/partials/card-index.blade.php
+++ resources/views/teams/partials/card-index.blade.php
@@ -7,7 +7,9 @@
         @endslot
 
         @slot('actions')
+            @can('create crm teams')
             <span class="float-right"><a type="button" class="btn btn-primary btn-sm" href="{{ url(route('laravel-crm.teams.create')) }}"><span class="fa fa-plus"></span> {{ ucfirst(__('laravel-crm::lang.add_team')) }}</a></span>
+            @endcan
         @endslot
 
     @endcomponent
@@ -34,13 +36,19 @@
                     <td>{{ $team->updated_at->toFormattedDateString() }}</td>
                     <td>{{ $team->users->count() }}</td>
                     <td class="disable-link text-right">
+                        @can('view crm teams')
                         <a href="{{  route('laravel-crm.teams.show',$team) }}" class="btn btn-outline-secondary btn-sm"><span class="fa fa-eye" aria-hidden="true"></span></a>
+                        @endcan
+                        @can('edit crm teams')
                         <a href="{{  route('laravel-crm.teams.edit',$team) }}" class="btn btn-outline-secondary btn-sm"><span class="fa fa-edit" aria-hidden="true"></span></a>
+                        @endcan
+                        @can('delete crm teams')
                         <form action="{{ route('laravel-crm.teams.destroy',$team) }}" method="POST" class="form-check-inline mr-0 form-delete-button">
                             {{ method_field('DELETE') }}
                             {{ csrf_field() }}
                             <button class="btn btn-danger btn-sm" type="submit" data-model="{{ __('laravel-crm::lang.team') }}"><span class="fa fa-trash-o" aria-hidden="true"></span></button>
                         </form>
+                        @endcan
                     </td>
                 </tr>
             @endforeach
