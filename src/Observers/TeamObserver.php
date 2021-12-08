e7e0444013ea0a159ef8415830b78008b2ca40ce
Andrew Drake <andrew@venturedrake.com>
2021-12-08 17:06:45 +1100

    Don't load team roles, settings, etc when not using teams mode

--- src/Observers/TeamObserver.php
+++ src/Observers/TeamObserver.php
@@ -31,90 +31,92 @@ class TeamObserver
     {
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
         
-        // Get the roles
-        foreach (DB::table('roles')
-                     ->where('crm_role', 1)
-                     ->whereNull('team_id')
-                     ->get() as $role) {
-            DB::table('roles')->updateOrInsert([
-                'name' => $role->name,
-                'guard_name' => $role->guard_name,
-                'description' => $role->description,
-                'crm_role' => $role->crm_role,
-                'team_id' => $team->id,
-            ], [
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
-            
-            if ($newRole = DB::table('roles')->where([
-                'name' => $role->name,
-                'guard_name' => $role->guard_name,
-                'description' => $role->description,
-                'crm_role' => $role->crm_role,
-                'team_id' => $team->id,
-            ])->first()) {
-                foreach (DB::table('permissions')
-                             ->leftJoin('role_has_permissions', 'permissions.id', '=', 'role_has_permissions.permission_id')
-                             ->where('role_has_permissions.role_id', $role->id)
-                             ->get() as $permission) {
-                    DB::table('role_has_permissions')->updateOrInsert([
-                        'permission_id' => $permission->id,
-                        'role_id' => $newRole->id,
-                    ]);
+        if(config('laravel-crm.teams')){
+            // Get the roles
+            foreach (DB::table('roles')
+                         ->where('crm_role', 1)
+                         ->whereNull('team_id')
+                         ->get() as $role) {
+                DB::table('roles')->updateOrInsert([
+                    'name' => $role->name,
+                    'guard_name' => $role->guard_name,
+                    'description' => $role->description,
+                    'crm_role' => $role->crm_role,
+                    'team_id' => $team->id,
+                ], [
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]);
+
+                if ($newRole = DB::table('roles')->where([
+                    'name' => $role->name,
+                    'guard_name' => $role->guard_name,
+                    'description' => $role->description,
+                    'crm_role' => $role->crm_role,
+                    'team_id' => $team->id,
+                ])->first()) {
+                    foreach (DB::table('permissions')
+                                 ->leftJoin('role_has_permissions', 'permissions.id', '=', 'role_has_permissions.permission_id')
+                                 ->where('role_has_permissions.role_id', $role->id)
+                                 ->get() as $permission) {
+                        DB::table('role_has_permissions')->updateOrInsert([
+                            'permission_id' => $permission->id,
+                            'role_id' => $newRole->id,
+                        ]);
+                    }
                 }
             }
-        }
 
-        if ($role = Role::where([
-            'name' => 'Owner',
-            'team_id' => $team->id,
-            'crm_role' => 1,
-        ])->first()) {
-            DB::table('model_has_roles')->insert([
-                'role_id' => $role->id,
-                'model_type' => auth()->user()->getMorphClass(),
-                'model_id' => auth()->user()->id,
+            if ($role = Role::where([
+                'name' => 'Owner',
                 'team_id' => $team->id,
-            ]);
-        }
+                'crm_role' => 1,
+            ])->first()) {
+                DB::table('model_has_roles')->insert([
+                    'role_id' => $role->id,
+                    'model_type' => auth()->user()->getMorphClass(),
+                    'model_id' => auth()->user()->id,
+                    'team_id' => $team->id,
+                ]);
+            }
 
-        foreach (DB::table('labels')
-                     ->whereNull('team_id')
-                     ->get() as $label) {
-            DB::table('labels')->insert([
-                'external_id' => Uuid::uuid4()->toString(),
-                'name' => $label->name,
-                'hex' => $label->hex,
-                'description' => $label->description,
-                'team_id' => $team->id,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
-        }
+            foreach (DB::table(config('laravel-crm.db_table_prefix').'labels')
+                         ->whereNull('team_id')
+                         ->get() as $label) {
+                DB::table(config('laravel-crm.db_table_prefix').'labels')->insert([
+                    'external_id' => Uuid::uuid4()->toString(),
+                    'name' => $label->name,
+                    'hex' => $label->hex,
+                    'description' => $label->description,
+                    'team_id' => $team->id,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]);
+            }
 
-        foreach (DB::table('organisation_types')
-                     ->whereNull('team_id')
-                     ->get() as $organisationType) {
-            DB::table('organisation_types')->insert([
-                'name' => $organisationType->name,
-                'description' => $organisationType->description,
-                'team_id' => $team->id,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
-        }
+            foreach (DB::table(config('laravel-crm.db_table_prefix').'organisation_types')
+                         ->whereNull('team_id')
+                         ->get() as $organisationType) {
+                DB::table(config('laravel-crm.db_table_prefix').'organisation_types')->insert([
+                    'name' => $organisationType->name,
+                    'description' => $organisationType->description,
+                    'team_id' => $team->id,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]);
+            }
 
-        foreach (DB::table('address_types')
-                     ->whereNull('team_id')
-                     ->get() as $addressType) {
-            DB::table('address_types')->insert([
-                'name' => $addressType->name,
-                'description' => $addressType->description,
-                'team_id' => $team->id,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
+            foreach (DB::table(config('laravel-crm.db_table_prefix').'address_types')
+                         ->whereNull('team_id')
+                         ->get() as $addressType) {
+                DB::table(config('laravel-crm.db_table_prefix').'address_types')->insert([
+                    'name' => $addressType->name,
+                    'description' => $addressType->description,
+                    'team_id' => $team->id,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]);
+            }
         }
     }
 
