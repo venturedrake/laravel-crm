9b48eaed21f34c78e3cb6194eb0e17eca06dd6c5
Andrew Drake <andrew@venturedrake.com>
2022-07-18 13:26:55 +1000

    Can't apply scope for teams when no user session

--- src/Scopes/BelongsToTeamsScope.php
+++ src/Scopes/BelongsToTeamsScope.php
@@ -18,7 +18,7 @@ class BelongsToTeamsScope implements Scope
     
     public function apply(Builder $builder, Model $model)
     {
-        if (config('laravel-crm.teams') && auth()->user()->currentTeam) {
+        if (config('laravel-crm.teams') && auth()->user() && auth()->user()->currentTeam) {
             $this->extend($builder);
             
             if (Schema::hasColumn($model->getTable(), 'global')) {
