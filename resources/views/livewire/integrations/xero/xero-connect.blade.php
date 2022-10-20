1c598f5b74f709fa6480d58c616a28e660cc545e
Andrew Drake <andrew@venturedrake.com>
2022-10-20 16:40:47 +1100

    Added settings to xero connect

--- resources/views/livewire/integrations/xero/xero-connect.blade.php
+++ resources/views/livewire/integrations/xero/xero-connect.blade.php
@@ -13,9 +13,40 @@
                         <div class="alert alert-info">
                             You are connected to the Xero organisation <strong>{{ $tenantName }}</strong>.
                         </div>
+                        <hr />
                         <a class="btn btn-success" href="{{ route('laravel-crm.integrations.xero.disconnect') }}">
                             Disconnect xero
                         </a>
+                        <hr />
+                        <h4 class="mb-3">Settings</h4>
+                        <table class="table mb-0 card-table table-hover">
+                            <tbody>
+                                <tr>
+                                    <td>Sync Contacts</td>
+                                    <td class="disable-link text-right">
+                                        <input id="sync_contacts" type="checkbox" name="sync_contacts" data-toggle="toggle" data-size="sm" data-on="Yes" data-off="No" data-onstyle="success" data-offstyle="danger">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Sync Products</td>
+                                    <td class="disable-link text-right">
+                                        <input id="sync_products" type="checkbox" name="sync_products" data-toggle="toggle" data-size="sm" data-on="Yes" data-off="No" data-onstyle="success" data-offstyle="danger">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Create & Update Quotes</td>
+                                    <td class="disable-link text-right">
+                                        <input id="create_quotes" type="checkbox" name="create_quotes" data-toggle="toggle" data-size="sm" data-on="Yes" data-off="No" data-onstyle="success" data-offstyle="danger">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Create & Update Invoices</td>
+                                    <td class="disable-link text-right">
+                                        <input id="create_invoices" type="checkbox" name="create_invoices" data-toggle="toggle" data-size="sm" data-on="Yes" data-off="No" data-onstyle="success" data-offstyle="danger">
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     @else
                         <a type="button" class="btn btn-outline-secondary" href="{{ route('laravel-crm.integrations.xero.connect') }}">
                             Connect to xero
