884f3c3690a043dd0b248452d414360d762faa77
Andrew Drake <andrew@venturedrake.com>
2022-11-11 17:32:37 +1100

    Fixed bug with z-index on toast notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     
 
     <!-- Styles -->
-    <link href="{{ asset('vendor/laravel-crm/css/app.css') }}?v=5675678678923" rel="stylesheet">
+    <link href="{{ asset('vendor/laravel-crm/css/app.css') }}?v=45365767868798 " rel="stylesheet">
 
     @livewireStyles
 </head>
