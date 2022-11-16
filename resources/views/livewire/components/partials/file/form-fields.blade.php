53e36837d0c5d4fee20255346ee5b6a1e6e09f1e
Andrew Drake <andrew@venturedrake.com>
2022-11-16 14:03:51 +1100

    Updated label

--- resources/views/livewire/components/partials/file/form-fields.blade.php
+++ resources/views/livewire/components/partials/file/form-fields.blade.php
@@ -1,6 +1,6 @@
 @include('laravel-crm::partials.form.file',[
   'name' => 'file',
-  'label' => ucfirst(__('laravel-crm::lang.file')),
+  'label' => ucfirst(__('laravel-crm::lang.add_file')),
   'attributes' => [
       'wire:model.defer' => 'file'  
   ]
