a3243797d57b4ff116cf27314e1762cd2064648a
Andrew Drake <andrew@venturedrake.com>
2023-02-28 10:12:15 +1100

    User relations trait

---
+++ src/Traits/HasCrmUserRelations.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace VentureDrake\LaravelCrm\Traits;
+
+trait HasCrmUserRelations
+{
+    public function createdByUser()
+    {
+        return $this->belongsTo(\App\User::class, 'user_created_id');
+    }
+
+    public function updatedByUser()
+    {
+        return $this->belongsTo(\App\User::class, 'user_updated_id');
+    }
+
+    public function deletedByUser()
+    {
+        return $this->belongsTo(\App\User::class, 'user_deleted_id');
+    }
+
+    public function restoredByUser()
+    {
+        return $this->belongsTo(\App\User::class, 'user_restored_id');
+    }
+
+    public function ownerUser()
+    {
+        return $this->belongsTo(\App\User::class, 'user_owner_id');
+    }
+}
