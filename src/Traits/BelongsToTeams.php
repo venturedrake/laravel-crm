22ae13a50a3a92994d92992be6d31b804039dfa7
Andrew Drake <andrew@venturedrake.com>
2023-03-23 17:29:46 +1100

    Fixed support for db seeder when using teams

--- src/Traits/BelongsToTeams.php
+++ src/Traits/BelongsToTeams.php
@@ -20,7 +20,7 @@ trait BelongsToTeams
         static::addGlobalScope(new BelongsToTeamsScope);
         
         static::creating(function (Model $model) {
-            if (config('laravel-crm.teams') && auth()->user()->currentTeam && ! $model->team_id) {
+            if (config('laravel-crm.teams') && auth()->hasUser() && auth()->user()->currentTeam && ! $model->team_id) {
                 $model->team_id = auth()->user()->currentTeam->id ?? null;
             }
         });
