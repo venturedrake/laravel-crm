05a64b19fa4cb3ef7de240270fab6902d1886194
Andrew Drake <andrew@venturedrake.com>
2024-02-07 22:34:16 +1100

    Email valid helper

---
+++ src/Http/Helpers/Validate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace VentureDrake\LaravelCrm\Http\Helpers\Validate;
+
+function email($email)
+{
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $address = $email;
+        $atPos = mb_strpos($address, '@');
+        $domain = mb_substr($address, $atPos + 1);
+        
+        if(checkdnsrr($domain . '.', 'MX')) {
+            return true;
+        }
+    }
+}
