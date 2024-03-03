ccd89fe2be259f94a0ab10e3d04e9331177c5db2
Andrew Drake <andrew@venturedrake.com>
2024-03-04 00:53:34 +1100

    Add default tax rate to products when creating

--- src/Services/OrderService.php
+++ src/Services/OrderService.php
@@ -272,12 +272,8 @@ class OrderService
 
     protected function addProduct($product, $request)
     {
-        $taxRate = TaxRate::where('default', 1)->first();
-
         $newProduct = Product::create([
             'name' => $product['product_id'],
-            'tax_rate_id' => $taxRate->id ?? null,
-            'tax_rate' => $taxRate->id ?? Setting::where('name', 'tax_rate')->first()->value ?? null,
             'user_owner_id' => $request->user_owner_id,
         ]);
 
