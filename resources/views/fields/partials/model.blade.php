d8f23907a5241879dea3120040a936afa0b067ef
Andrew Drake <andrew@venturedrake.com>
2024-04-05 15:56:31 +1100

    Added custom  field inputs

--- resources/views/fields/partials/model.blade.php
+++ resources/views/fields/partials/model.blade.php
@@ -19,9 +19,30 @@
             @include('laravel-crm::partials.form.select',[
                'name' => 'fields['.$fieldValue->field->id.']',
                'label' => ucfirst(__($fieldValue->field->name)),
-                'options' => [],
+               'options' => ['' => ''] + $fieldValue->field->fieldOptions->pluck('label','id')->toArray(),
                'value' => old('fields['.$fieldValue->field->id.']', $fieldValue->value ?? null) 
            ])
             @break
+        @case('checkbox')
+            @include('laravel-crm::partials.form.checkbox',[
+               'name' => 'fields['.$fieldValue->field->id.']',
+               'label' => ucfirst(__($fieldValue->field->name)),
+               'value' => old('fields['.$fieldValue->field->id.']', $fieldValue->value ?? null) 
+           ])
+            @break
+        @case('checkbox_multiple')
+            <x-form-group label="{{ ucfirst(__($fieldValue->field->name)) }}">
+                @foreach($fieldValue->field->fieldOptions as $fieldOption)
+                    <x-form-checkbox name="fields[{{ $fieldValue->field->id }}]" value="{{ $fieldOption->id }}" label="{{ $fieldOption->label }}" />
+                @endforeach
+            </x-form-group>
+            @break
+        @case('radio')
+            <x-form-group name="fields[{{ $fieldValue->field->id }}]" label="{{ ucfirst(__($fieldValue->field->name)) }}">
+                @foreach($fieldValue->field->fieldOptions as $fieldOption)
+                    <x-form-radio name="fields[{{ $fieldValue->field->id }}]" value="{{ $fieldOption->id }}" label="{{ $fieldOption->label }}" />
+                @endforeach
+            </x-form-group>
+         @break
     @endswitch
 @endforeach  
