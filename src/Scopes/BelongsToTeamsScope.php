223afacd649f43d150e232861cf23765e8475722
Andrew Drake <andrew@venturedrake.com>
2024-05-14 22:13:18 +1000

    Add a bypass on scope for slow Schema::hasColumn() method

--- src/Scopes/BelongsToTeamsScope.php
+++ src/Scopes/BelongsToTeamsScope.php
@@ -17,13 +17,22 @@ class BelongsToTeamsScope implements Scope
      */
     protected $extensions = ['AllTeams'];
 
+    /**
+     * Bypass the slow Schema::hasColumn() method for these tables.
+     *
+     * @var string[]
+     */
+    protected $include = [
+        'settings'
+    ];
+
     public function apply(Builder $builder, Model $model)
     {
         if (config('laravel-crm.teams') && auth()->hasUser() && auth()->user()->currentTeam) {
             if(! config('nova.path') || (config('nova.path') && ! Str::startsWith(request()->getRequestUri(), config('nova.path')))) {
                 $this->extend($builder);
-
-                if (Schema::hasColumn($model->getTable(), 'global')) {
+                
+                if (in_array($model->getTable(), $this->include) || Schema::hasColumn($model->getTable(), 'global')) {
                     $builder->where(function ($query) use ($model) {
                         $query->orWhere($model->getTable().'.team_id', auth()->user()->currentTeam->id)
                             ->orWhere($model->getTable().'.global', 1);
