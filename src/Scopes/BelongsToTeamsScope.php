e9e140c43e8ea74d03886dae5c5110bdd9620178
Andrew Drake <andrew@venturedrake.com>
2024-05-16 16:28:00 +1000

    Added setting for models with global override

--- src/Scopes/BelongsToTeamsScope.php
+++ src/Scopes/BelongsToTeamsScope.php
@@ -5,7 +5,6 @@ namespace VentureDrake\LaravelCrm\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 class BelongsToTeamsScope implements Scope
@@ -17,22 +16,13 @@ class BelongsToTeamsScope implements Scope
      */
     protected $extensions = ['AllTeams'];
 
-    /**
-     * Bypass the slow Schema::hasColumn() method for these tables.
-     *
-     * @var string[]
-     */
-    protected $include = [
-        'settings'
-    ];
-
     public function apply(Builder $builder, Model $model)
     {
         if (config('laravel-crm.teams') && auth()->hasUser() && auth()->user()->currentTeam) {
             if(! config('nova.path') || (config('nova.path') && ! Str::startsWith(request()->getRequestUri(), config('nova.path')))) {
                 $this->extend($builder);
-                
-                if (in_array($model->getTable(), $this->include) || Schema::hasColumn($model->getTable(), 'global')) {
+
+                if (in_array($model->getTable(), config('laravel-crm.model_with_global'))) {
                     $builder->where(function ($query) use ($model) {
                         $query->orWhere($model->getTable().'.team_id', auth()->user()->currentTeam->id)
                             ->orWhere($model->getTable().'.global', 1);
