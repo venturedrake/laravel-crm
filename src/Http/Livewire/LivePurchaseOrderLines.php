602a677cae98f0fb0d3bb770bb397860c32a3180
Andrew Drake <andrew@venturedrake.com>
2024-06-14 23:55:12 +1000

    Tax on purchase order lines

--- src/Http/Livewire/LivePurchaseOrderLines.php
+++ src/Http/Livewire/LivePurchaseOrderLines.php
@@ -32,6 +32,10 @@ class LivePurchaseOrderLines extends Component
 
     public $quantity;
 
+    public $tax_amount;
+
+    public $tax_rate;
+
     public $amount;
 
     public $comments;
@@ -86,6 +90,7 @@ class LivePurchaseOrderLines extends Component
                 }
 
                 $this->price[$this->i] = $old['price'] ?? null;
+                $this->tax_amount[$this->i] = $old['tax_amount'] ?? null;
                 $this->amount[$this->i] = $old['amount'] ?? null;
                 $this->comments[$this->i] = $old['comments'] ?? null;
             }
@@ -111,6 +116,7 @@ class LivePurchaseOrderLines extends Component
                 }
 
                 $this->price[$this->i] = $purchaseOrderLine->price / 100;
+                $this->tax_amount[$this->i] = $purchaseOrderLine->tax_amount / 100;
                 $this->amount[$this->i] = $purchaseOrderLine->amount / 100;
                 $this->comments[$this->i] = $purchaseOrderLine->comments;
             }
@@ -127,6 +133,7 @@ class LivePurchaseOrderLines extends Component
         $this->i = $i;
         $this->price[$i] = null;
         $this->quantity[$i] = null;
+        $this->tax_rate[$i] = null;
         array_push($this->inputs, $i);
 
         $this->dispatchBrowserEvent('addedItem', ['id' => $this->i]);
@@ -154,8 +161,12 @@ class LivePurchaseOrderLines extends Component
 
         for ($i = 1; $i <= $this->i; $i++) {
             if (isset($this->product_id[$i])) {
-                if($product = \VentureDrake\LaravelCrm\Models\Product::find($this->product_id[$i])) {
-                    $taxRate = $product->taxRate->rate ?? $product->tax_rate ?? 0;
+                $product = \VentureDrake\LaravelCrm\Models\Product::find($this->product_id[$i]);
+
+                if($product && $product->taxRate) {
+                    $taxRate = $product->taxRate->rate;
+                } elseif($product && $product->tax_rate) {
+                    $taxRate = $product->tax_rate;
                 } elseif($taxRate = TaxRate::where('default', 1)->first()) {
                     $taxRate = $taxRate->rate;
                 } elseif($taxRate = $this->settingService->get('tax_rate')) {
@@ -164,15 +175,18 @@ class LivePurchaseOrderLines extends Component
                     $taxRate = 0;
                 }
 
+                $this->tax_rate[$i] = $taxRate;
+
                 if (is_numeric($this->price[$i]) && is_numeric($this->quantity[$i])) {
                     $this->amount[$i] = $this->price[$i] * $this->quantity[$i];
                     $this->price[$i] = $this->currencyFormat($this->price[$i]);
+                    $this->tax_amount[$i] = $this->currencyFormat($this->amount[$i] * ($taxRate / 100));
                 } else {
                     $this->amount[$i] = 0;
                 }
 
-                $this->sub_total += $this->amount[$i];
-                $this->tax += $this->amount[$i] * ($taxRate / 100);
+                $this->sub_total += round($this->amount[$i], 2);
+                $this->tax += round($this->amount[$i] * ($taxRate / 100), 2);
                 $this->amount[$i] = $this->currencyFormat($this->amount[$i]);
             }
         }
