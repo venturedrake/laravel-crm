d415e9ed07f9668a41ca3ed95056cca329846616
JalalLinuX <smjjalalzadeh93@gmail.com>
2024-08-20 16:43:26 +0330

    Create auth.php

---
+++ resources/lang/fa/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'اطلاعات وارد شده صحیح نمی باشد.',
+    'password' => 'رمزعبور صحیح نیست.',
+    'throttle' => 'درخواست بیش از حد مجاز! لطفا بعد از :seconds ثانیه دوباره امتحان کنید.',
+    'verification_link_sent' => 'لینک تاییدیه ایمیل، ارسال شد.',
+    'email_not_verified' => 'ایمیل شما تایید نشده است.',
+
+];
