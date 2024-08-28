2e10cf33126c94d9ea5baa80e04fbe46b90f15f7
JT <jonathan8312@gmail.com>
2024-08-28 10:59:58 -0500

    Feature: Add Spanish (Colombia) translations
    Added and updated localization files to include complete translations for Spanish as spoken in Colombia, covering all current application text strings.

---
+++ resources/lang/es_CO/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de Idioma para Paginación
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son utilizadas por la biblioteca de paginador para construir
+    | los enlaces de paginación simples. Eres libre de cambiarlas por lo que
+    | desees para personalizar tus vistas y que se ajusten mejor a tu aplicación.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Siguiente &raquo;',
+
+];
