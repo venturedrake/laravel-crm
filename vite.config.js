01f51908f242793e298694089a6d7c5f4ae6aeb6
Andrew Drake <andrew@venturedrake.com>
2024-12-03 19:05:05 +1100

    Vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            outDir: '../assets',
             refresh: true,
         }),
     ],
