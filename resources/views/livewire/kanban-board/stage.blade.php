32796b4cc0d6bd3928b04ed09dbb1a97525e857b
Andrew Drake <andrew@venturedrake.com>
2025-03-05 21:48:16 +1100

    Fixed board layout with add item button at bottom of stage

--- resources/views/livewire/kanban-board/stage.blade.php
+++ resources/views/livewire/kanban-board/stage.blade.php
@@ -2,7 +2,7 @@
 <div class="flex flex-col flex-shrink-0 w-80 card bg-base-300 rounded-lg py-4 px-5 shadow-sm">
     <div class="card-header bg-light">
         <h3 class="card-title h5 mb-4">
-            {{ $stage['name'] }}
+            {{ $stage['name'] }} ({{ count($stage['records']) }})
         </h3>
         @isset($stage['description'] )
             <small class="mb-0 text-muted">
@@ -10,16 +10,18 @@
             </small>
         @endisset
     </div>
-    <div class="card-body p-0" id="{{ $stage['stageRecordsId'] }}" data-stage-id="{{ $stage['id'] }}">
-     @foreach($stage['records'] as $record)
-         @include('laravel-crm::livewire.kanban-board.record', [
-             'record' => $record,
-         ])
-     @endforeach
-    </div>
-    <div class="card-footer">
-        <a href="{{ url(route('laravel-crm.'.\Illuminate\Support\Str::plural($model).'.create', ['stage' => $stage['id']])) }}" class="btn btn-primary btn-block text-white mt-2">Add
-            {{ $model }}</a>
+    <div class="card-body p-0">
+        <span id="{{ $stage['stageRecordsId'] }}" data-stage-id="{{ $stage['id'] }}">
+            @foreach($stage['records'] as $record)
+                @include('laravel-crm::livewire.kanban-board.record', [
+                    'record' => $record,
+                ])
+            @endforeach
+        </span>
+        <div class="card-footer">
+         <a href="{{ url(route('laravel-crm.'.\Illuminate\Support\Str::plural($model).'.create', ['stage' => $stage['id']])) }}" class="btn btn-primary btn-block text-white">Add
+             {{ $model }}</a>
+        </div>
     </div>
 </div>
 
