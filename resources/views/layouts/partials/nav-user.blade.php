4f22ae59237611e9147d04bd61d5050d8c96d702
Andrew Drake <andrew@venturedrake.com>
2025-04-07 16:54:37 +1000

    Support for jetstream teams

--- resources/views/layouts/partials/nav-user.blade.php
+++ resources/views/layouts/partials/nav-user.blade.php
@@ -1,3 +1,43 @@
+<!-- Teams Dropdown -->
+@if (class_exists('\Laravel\Jetstream\Jetstream') && Laravel\Jetstream\Jetstream::hasTeamFeatures())
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->currentTeam->name }} <span class="caret"></span>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <!-- Team Management -->
+            <div class="block px-4 py-2 text-xs text-gray-400">
+                {{ __('Manage Team') }}
+            </div>
+
+            <!-- Team Settings -->
+            <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                {{ __('Team Settings') }}
+            </x-dropdown-link>
+
+            @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
+                <x-dropdown-link href="{{ route('teams.create') }}">
+                    {{ __('Create New Team') }}
+                </x-dropdown-link>
+            @endcan
+
+            <!-- Team Switcher -->
+            @if (Auth::user()->allTeams()->count() > 1)
+                <div class="border-t border-gray-200"></div>
+
+                <div class="block px-4 py-2 text-xs text-gray-400">
+                    {{ __('Switch Teams') }}
+                </div>
+
+                @foreach (Auth::user()->allTeams() as $team)
+                    <x-switchable-team :team="$team" />
+                @endforeach
+            @endif
+        </div>
+    </li>
+@endif    
+
 <li class="nav-item dropdown">
     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
         {{ Auth::user()->name }} <span class="caret"></span>
