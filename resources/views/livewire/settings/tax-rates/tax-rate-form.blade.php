19d999e7715e91b3c172d40e4fab3546e9655110
Andrew Drake <andrew@venturedrake.com>
2026-01-06 11:34:11 +1100

    Added required attribute to some settings forms

--- resources/views/livewire/settings/tax-rates/tax-rate-form.blade.php
+++ resources/views/livewire/settings/tax-rates/tax-rate-form.blade.php
@@ -1,7 +1,7 @@
 <x-mary-card separator>
     <div wire:key="tax-rate" class="space-y-3">
-        <x-mary-input wire:model="name" label="{{ ucfirst(__('laravel-crm::lang.name')) }}" />
-        <x-mary-input wire:model="rate" label="{{ ucfirst(__('laravel-crm::lang.rate')) }}"  suffix="%" />
+        <x-mary-input wire:model="name" label="{{ ucfirst(__('laravel-crm::lang.name')) }}" required />
+        <x-mary-input wire:model="rate" label="{{ ucfirst(__('laravel-crm::lang.rate')) }}" suffix="%" required />
         <x-mary-textarea wire:model="description" label="{{ ucfirst(__('laravel-crm::lang.description')) }}" rows="5" />
         <x-mary-toggle wire:model="default" label="{{ ucfirst(__('laravel-crm::lang.default_tax_rate')) }}" />
         <x-mary-input wire:model="tax_type" label="{{ ucfirst(__('laravel-crm::lang.tax_type')) }}" />
