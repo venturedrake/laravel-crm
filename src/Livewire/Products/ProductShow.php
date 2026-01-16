44aa8db7f756528df25487f9af3158a2c9099497
Andrew Drake <andrew@venturedrake.com>
2026-01-16 11:42:50 +1100

    Fixed product delete on show page

--- src/Livewire/Products/ProductShow.php
+++ src/Livewire/Products/ProductShow.php
@@ -3,11 +3,23 @@
 namespace VentureDrake\LaravelCrm\Livewire\Products;
 
 use Livewire\Component;
+use Mary\Traits\Toast;
 use VentureDrake\LaravelCrm\Models\Product;
 
 class ProductShow extends Component
 {
+    use Toast;
+    
     public Product $product;
+
+    public function delete($id)
+    {
+        if ($product = Product::find($id)) {
+            $product->delete();
+
+            $this->success(ucfirst(trans('laravel-crm::lang.product_deleted')), redirectTo: route('laravel-crm.products.index'));
+        }
+    }
 
     public function render()
     {
