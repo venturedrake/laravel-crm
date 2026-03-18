ffde73601c4bbd132dc3b571d1a83eadb430a413
Andrew Drake <andrew@venturedrake.com>
2026-03-19 00:04:40 +1100

    Fixed modal in card on board bug

--- resources/views/livewire/kanban-board/record.blade.php
+++ resources/views/livewire/kanban-board/record.blade.php
@@ -23,7 +23,6 @@
                         <x-mary-menu-item link="{{ route('laravel-crm.leads.edit', ['lead' => $record['id']]) }}" title="{{ ucfirst(__('laravel-crm::lang.edit')) }}" />
                         <x-mary-menu-item onclick="modalDeleteLead{{ $record['id'] }}.showModal()" title="{{ ucfirst(__('laravel-crm::lang.delete')) }}" />
                     </x-mary-dropdown>
-                    <x-crm-delete-confirm model="lead" id="{{ $record['id'] }}" />
                     @break
                 @endswitch
             </div>
@@ -40,32 +39,4 @@
         </div>
     </div>
 </div>
-
-{{--
-<div class="card mb-3 cursor-grab" id="{{ $record['id'] }}">
---}}
-   {{-- <x-mary-card title="Your stats" subtitle="Our findings about you" shadow separator>
-        I have title, subtitle, separator and shadow.
-    </x-mary-card>--}}
-
-    
-    {{--<div class="card-body p-3 text-wrap">
-        <h5 class="mb-1">{{ $record['title'] }}</h5>
-        @foreach($record['labels'] as $label)
-            <x-mary-badge value="{{ $label->name }}" class="text-white" style="border-color: #{{ $label->hex }}; background-color: #{{ $label->hex }}" />
-        @endforeach
-        --}}{{--@include('laravel-crm::partials.labels',[
-            'labels' => $record['labels'],
-            'limit' => 3
-        ])--}}{{--
-        <div class="mt-2">
-            <a href="{{ url(route('laravel-crm.'.\Illuminate\Support\Str::plural($model).'.show', $record['id'])) }}">{{ $record['number'] }}</a>
-        </div>
-        <div class="mt-2">
-            @if($record['amount'])
-            {{ money($record['amount'], $record['currency']) }}
-            @endif
-            <div class="mb-0 d-inline-block float-right"><i class="fa fa-user-circle" aria-hidden="true"></i></div>
-        </div>
-    </div>--}}
-{{--</div>--}}
+<x-crm-delete-confirm model="lead" id="{{ $record['id'] }}" />
