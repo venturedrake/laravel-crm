824cfcae935f9dfcdd00b0eed657422c61d8dc18
Andrew Drake <andrew@venturedrake.com>
2026-05-16 18:09:32 +1000

    Fix null crash on updates page when version settings not seeded

--- resources/views/updates/index.blade.php
+++ resources/views/updates/index.blade.php
@@ -2,15 +2,29 @@
     <div class="crm-content">
         <x-mary-header title="Laravel CRM {{ ucfirst(__('laravel-crm::lang.updates')) }}" progress-indicator></x-mary-header>
         <x-mary-card shadow>
+            @php
+                $currentVersion  = \VentureDrake\LaravelCrm\Models\Setting::where('name', 'version')->first()?->value;
+                $latestVersion   = \VentureDrake\LaravelCrm\Models\Setting::where('name', 'version_latest')->first()?->value;
+                $isLatest        = $currentVersion && $latestVersion && $currentVersion >= $latestVersion;
+            @endphp
             <div class="grid gap-y-3">
-                <p>Your current version {{ ucfirst(__('laravel-crm::lang.current_version')) }} {{ \VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value }} {{ (\VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value == \VentureDrake\LaravelCrm\Models\Setting::where('name','version_latest')->first()->value) ? __('laravel-crm::lang.is_the_latest_version') : null }} is latest. </p>
-                            @if(\VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value < \VentureDrake\LaravelCrm\Models\Setting::where('name','version_latest')->first()->value)
-                <p>{{ ucfirst(__('laravel-crm::lang.updated_version_of_laravel_crm_is_available')) }}</p>
-                <p class="card-text">{{ ucfirst(__('laravel-crm::lang.you_can_update_from_laravel_crm')) }} {{ \VentureDrake\LaravelCrm\Models\Setting::where('name','version')->first()->value }} to {{ \VentureDrake\LaravelCrm\Models\Setting::where('name','version_latest')->first()->value }}</p>
-                <p>
-                    <a type="button" class="btn btn-primary text-white" href="https://github.com/venturedrake/laravel-crm" target="_blank">{{ ucfirst(__('laravel-crm::lang.upgrade_guide')) }}</a>
-                </p>
-            @endif
+                @if($currentVersion)
+                    <p>{{ ucfirst(__('laravel-crm::lang.current_version')) }}: <strong>{{ $currentVersion }}</strong>
+                        @if($isLatest)
+                            &mdash; {{ __('laravel-crm::lang.is_the_latest_version') }}
+                        @endif
+                    </p>
+                @else
+                    <p>{{ ucfirst(__('laravel-crm::lang.current_version')) }}: &mdash;</p>
+                @endif
+
+                @if($currentVersion && $latestVersion && $currentVersion < $latestVersion)
+                    <p>{{ ucfirst(__('laravel-crm::lang.updated_version_of_laravel_crm_is_available')) }}</p>
+                    <p>{{ ucfirst(__('laravel-crm::lang.you_can_update_from_laravel_crm')) }} {{ $currentVersion }} to {{ $latestVersion }}</p>
+                    <p>
+                        <a type="button" class="btn btn-primary text-white" href="https://github.com/venturedrake/laravel-crm" target="_blank">{{ ucfirst(__('laravel-crm::lang.upgrade_guide')) }}</a>
+                    </p>
+                @endif
             </div>
         </x-mary-card>
     </div>
